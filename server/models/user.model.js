b1b5e0e762b3d47b053329be9085fcf4af37dd24
Harsh <gamerpandeyharsh@gmail.com>
2025-11-19 15:45:55 +0530

    added user Model schema

---
+++ server/models/user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (email) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+      },
+      message: "Invalid email format",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+});
+
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
