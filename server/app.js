e4f801aa0981804011134ae38e552a95ed67bab1
Harsh <gamerpandeyharsh@gmail.com>
2025-11-20 07:28:09 +0530

    Updated Logic

--- server/app.js
+++ server/app.js
@@ -1,11 +1,10 @@
-import express from 'express';
-import mongoose from 'mongoose';
-
+import express from "express";
+import UserRouter from "./routes/user.route.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/users", UserRouter);
 
-
-export default app
+export default app;
