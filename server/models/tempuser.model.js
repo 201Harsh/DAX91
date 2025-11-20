36d6704776f312233d0140c441f563e5fdad80cb
Harsh <gamerpandeyharsh@gmail.com>
2025-11-20 08:53:12 +0530

    Updateed TempUser Model

---
+++ server/models/tempuser.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const TempUserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (email) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+      },
+      message: "Invalid email format",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  otp: {
+    type: String,
+    required: true,
+  },
+  otpExpiry: {
+    type: Date,
+    required: true,
+  },
+});
+
+TempUserSchema.index({ otpExpiry: 1 }, { expireAfterSeconds: 0 });
+
+const TempUser = mongoose.model("TempUser", TempUserSchema);
+
+export default TempUser;
