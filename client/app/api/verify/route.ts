6bf69a5ff6120aedfcd61d661893bf530ee133d1
Harsh <gamerpandeyharsh@gmail.com>
2025-11-22 14:52:52 +0530

    added Verify otp proxy Route

---
+++ client/app/api/verify/route.ts
@@ -0,0 +1,38 @@
+import Axiosinstance from "@/config/AxiosInstance";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { email, otp } = body;
+
+    const response = await Axiosinstance.post("/users/verify", {
+      email,
+      otp,
+    });
+
+    const NextRes = NextResponse.json(response.data, {
+      status: response.status,
+    });
+
+    const cookies: any = response.headers["set-cookie"];
+    if (cookies) {
+      NextRes.headers.set("set-cookie", cookies);
+    }
+
+    return NextRes;
+  } catch (error: any) {
+    if (error.response) {
+      return Response.json(
+        { error: error.response.data.message },
+        { status: error.response.status }
+      );
+    }
+    return Response.json(
+      {
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
